2f03bf52d3b1e8c2e97823e9d34aba83f3652881
Jeff Tarr <jeff@tarr.com>
2013-12-12 16:16:48 -0500

    Updated code for class presentation. Removed LEDs. Added waterproof sensor. Added better Gmail retry code.

--- TSCU_Wx.py
+++ TSCU_Wx.py
@@ -2,64 +2,55 @@
 #
 # TSCU Weather Node script - based on AdaFruit demo code and modified by Jeff Tarr 26-Nov-2013
 
+import os
+import glob
 import subprocess
 import re
 import sys
 import time
 import datetime
 import gspread
-import RPi.GPIO as GPIO
 from Adafruit_BMP085 import BMP085
-import gps
 from config import *
 
-# Setup LEDs
-GPIO.setmode(GPIO.BCM)
-GREEN_LED = 23
-RED_LED = 18
-GPIO.setup(GREEN_LED, GPIO.OUT)
-GPIO.setup(RED_LED, GPIO.OUT)
-GPIO.output(GREEN_LED, False)
-GPIO.output(RED_LED, True)
+# Setup DS18B20 sensor
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+ 
+def read_temp_raw():
+	catdata = subprocess.Popen(['cat',device_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out,err = catdata.communicate()
+	out_decode = out.decode('utf-8')
+	lines = out_decode.split('\n')
+	return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
 
 # Setup BMP pressure sensor
 bmp = BMP085(0x77)
 
-# Setup GPS sensor
-gpssession = gps.gps("localhost", "2947")
-gpssession.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-
-# Login with your Google account
-try:
-  gc = gspread.login(email, password)
-except:
-  print "Unable to log in.  Check your email address/password"
-  sys.exit()
-
-# Open a worksheet from your spreadsheet using the filename
-try:
-  worksheet = gc.open(spreadsheet).sheet1
-  # Alternatively, open a spreadsheet using the spreadsheet's key
-  #worksheet = gc.open_by_key('0AldnNT5XbvdpPYRGUHZUcnd5ZHVTOUx5N0VYQUE')
-except:
-  print "Unable to open the spreadsheet.  Check your filename: %s" % spreadsheet
-  sys.exit()
-
 # Continuously append data
 while(True):
+  # Login with your Google account
   try:
-	report = gpssession.next()
-	print report
-  except KeyError:
-	pass
-  except KeyboardInterrupt:
-	quit()
-  except StopIteration:
-	gpssession= None
-	print "GPSD has terminated"
+    gc = gspread.login(email, password)
+  except:
+    print "Unable to log in.  Check your email address/password"
 
-
-  output = subprocess.check_output(["./Adafruit_DHT", "2302", "4"]);
+  output = subprocess.check_output(["./Adafruit_DHT", "2302", "22"]);
 
   matches = re.search("Temp =\s+([0-9.]+)", output)
   if (not matches):
@@ -75,8 +66,12 @@ while(True):
 	continue
   humidity = float(matches.group(1))
 
-  print "Temperature: %.1f F" % temp
-  print "Humidity:    %.1f %%" % humidity
+  print "2302 Temperature: %.1f F" % temp
+  print "2302 Humidity:    %.1f %%" % humidity
+
+  deg_c, deg_f = read_temp()
+
+  print "28 Temperature: %.1f F" % deg_f
 
   # Get data from BMP sensor
   bmptemp = bmp.readTemperature() * 9/5 + 32
@@ -88,23 +83,17 @@ while(True):
   print "BPM Pressure %.2f hPa" % bmppressure
   print "BPM Altitude %.2f" % bmpaltitude
 
-  # Get data from GPS
-  # This is coming...
-
-  gpsaltitude = 100
-  gpslatitude = 40.803775
-  gpslongitude = -73.966282
-  gpsvisiblesats = 20
 
   # Append the data in the spreadsheet, including a timestamp
   try:
-    values = [datetime.datetime.now(), temp, humidity, bmppressure, bmptemp, bmpaltitude, gpslatitude, gpslongitude, gpsaltitude, gpsvisiblesats]
+    worksheet = gc.open(spreadsheet).sheet1
+    values = [datetime.datetime.now(), temp, humidity, bmppressure, bmptemp, bmpaltitude, deg_f]
     worksheet.append_row(values)
+
   except:
     print "Unable to append data.  Check your connection?"
-    sys.exit()
 
-  # Wait 30 seconds before continuing
+  # Wait 60 seconds before continuing
   print "Posted: ",
   print (values)
   time.sleep(60)
